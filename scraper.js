8790de8189f72ef912fe8af57ecc2b59f391d9d3
HaLan0101 <dnhalan0101@gmail.com>
2023-11-16 14:56:37 +0100

    save

--- scraper.js
+++ scraper.js
@@ -1,40 +1,41 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const { CronJob } = require('cron');
+const {CronJob} = require('cron');
 
+// node scraper.js
 async function scrapeOnePiece() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    await page.goto('https://onepiecescan.fr/');
+  await page.goto('https://onepiecescan.fr/');
 
-    const content = await page.content();
-    const $ = cheerio.load(content);
+  const content = await page.content();
+  const $ = cheerio.load(content);
 
-    const firstLi = $('#ceo_latest_comics_widget-3 ul li').first();
+  const firstLi = $('#ceo_latest_comics_widget-3 ul li').first();
 
-    const latestChapterLink = firstLi.find('a').attr('href');
+  const latestChapterLink = firstLi.find('a').attr('href');
 
-    await browser.close();
+  await browser.close();
 
-    return latestChapterLink;
+  return latestChapterLink;
 }
 
 function saveLatestChapterLink() {
   scrapeOnePiece()
-    .then((latestChapterLink) => {
-      const data = { latestChapterLink };
+    .then(latestChapterLink => {
+      const data = {latestChapterLink};
       fs.writeFileSync('latest_chapter.json', JSON.stringify(data, null, 2));
       console.log('Dernier chapitre de One Piece :', latestChapterLink);
     })
-    .catch((error) => {
+    .catch(error => {
       console.error('Erreur lors du scraping :', error);
     });
 }
 
 //CronJob pour planifier le scraping tous les minutes
-const job = new CronJob('* * * * *', function() {
+const job = new CronJob('* * * * *', function () {
   console.log('Exécution du scraping...');
   saveLatestChapterLink();
 });
